0af68379874d51cda9d3567db412d5bc9afeebc1
Gianluca <gianlucazin1990@gmail.com>
2020-11-28 17:30:04 +0100

    first deployment

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const cors = require('cors');
+const url = require('url');
+const { json } = require('body-parser');
+const { Schema } = mongoose;
+
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+  console.log('we are conncected to the db!');
+});
+
+// console.log(mongoose.connection.readyState);
+
+const urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true,
+  },
+  short_url: String,
+});
+
+const Url = mongoose.model('Url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,16 +40,42 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl/new', function (req, res, next) {
+  const input = req.body.url;
+  const reg = /^https:\/\//;
+  if (!reg.test(input)) return res.status(400).json({ error: 'invalid URL' });
+  const url = new URL(input);
+
+  dns.lookup(url.host, async (err, address, family) => {
+    if (err) {
+      res.status(400).json({ error: 'invalid URL' });
+    } else {
+      console.log('address: %j family: IPv%s', address, family);
+      const newUrl = await Url.create({ original_url: url.href });
+      res.status(201).json({ original_url: url.href, short_url: newUrl._id });
+    }
+  });
+});
+
+app.get('/api/shorturl/:id', function (req, res) {
+  const id = req.params.id;
+  Url.findById(id, function (err, url) {
+    if (err) return console.error(err);
+    res.redirect(url.original_url);
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
